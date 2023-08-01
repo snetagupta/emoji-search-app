30dd79dcfa416c094b0b45cb797a2b4c736d7fec
snetagupta <snetagupta4nov@gmail.com>
2023-08-01 14:56:08 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState } from 'react';
+import emojiData from './emojis.json';
+
+
+const App = () => {
+  const [search,setSearch] = useState('');
+  
+  const filteredEmojis = emojiData.filter((emoji)=>(emoji.name.includes(search )));
+  
+
+  const handleSearchChange = (event)=>{
+    setSearch(event.target.value);
+  };
+  
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className='bg-teal-100 h-screen'>
+    <div className="text-center  space-y-7">
+      <h1 className='text-3xl pt-12'>Emoji search</h1>
+      <input className='border-2 border-gray-500 w-80 p-2'
+      type="text" placeholder="Search emoji here" name="search" value={search}
+      onChange={handleSearchChange}/>
+      <div className='m-2'>
+       {search ? filteredEmojis.map((emoji) => <div className='border-2 rounded-md border-gray-500 w-20 p-1 m-auto mt-4 bg-white' >{emoji.emoji}</div>) : ""}
+       </div>
+    </div>
     </div>
   );
-}
+};
 
 export default App;
